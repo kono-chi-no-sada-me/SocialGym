6acc0bccae9332c348246ff90e341d572b645106
Daniel Boll <43689101+Daniel-Boll@users.noreply.github.com>
2019-05-03 15:16:00 -0300

    Add files via upload

---
+++ Social Gym/public/js/app.js
@@ -0,0 +1,18 @@
+// <!-- The core Firebase JS SDK is always required and must be listed first -->
+
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+//     //  https://firebase.google.com/docs/web/setup#config-web-app -->
+
+
+  // Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBfA9s1WFOB1csT9b5lxg4jUUXYgR-sq_0",
+    authDomain: "social-gym-eb33.firebaseapp.com",
+    databaseURL: "https://social-gym-eb33.firebaseio.com",
+    projectId: "social-gym-eb33",
+    storageBucket: "social-gym-eb33.appspot.com",
+    messagingSenderId: "1068366452926",
+    appId: "1:1068366452926:web:f5bdf4c2723a3384"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
